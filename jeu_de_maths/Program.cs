baa0206ba74d787166247b01c424142cf78c6815
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-12-21 08:46:49 +0100

    Ajoutez des fichiers projet.

---
+++ jeu_de_maths/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace jeu_de_maths
+{
+    class Program
+    {
+        static void PoserQuestion()
+        {
+            int reponseInt = 0;
+
+            Console.WriteLine("5 + 2 = ");
+            string reponse = Console.ReadLine();
+
+            try
+            {
+                reponseInt = int.Parse(reponse);
+
+            }
+            catch 
+            {
+                Console.WriteLine("ERREUR: Vous devez rentrer un nombre");
+            }
+
+        } 
+
+
+        static void Main(string[] args)
+        {
+            PoserQuestion();
+        }
+    }
+}
